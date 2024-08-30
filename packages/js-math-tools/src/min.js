a3aa95abb2a19174f8d495937de8eb3db7b66db1
jrc03c <jrc03c@pm.me>
2024-08-30 14:26:03 -0500

    ...

--- packages/js-math-tools/src/min.js
+++ packages/js-math-tools/src/min.js
@@ -1,33 +1,7 @@
-const assert = require("./assert")
-const flatten = require("./flatten")
-const isArray = require("./is-array")
-const isDataFrame = require("./is-dataframe")
-const isSeries = require("./is-series")
+const stats = require("./stats")
 
 function min(arr) {
-  if (isDataFrame(arr) || isSeries(arr)) {
-    return min(arr.values)
-  }
-
-  assert(
-    isArray(arr),
-    "The `min` function only works on arrays, Series, and DataFrames!"
-  )
-
-  try {
-    arr = flatten(arr)
-    let lowest = Infinity
-
-    for (const v of arr) {
-      if (v < lowest) {
-        lowest = v
-      }
-    }
-
-    return lowest
-  } catch (e) {
-    return NaN
-  }
+  return stats(arr).min
 }
 
 module.exports = min
