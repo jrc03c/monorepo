19b18e3762748c5dbe819007213f170f610380ce
jrc03c <jrc03c@pm.me>
2024-08-30 14:36:23 -0500

    ...

--- packages/js-math-tools/src/variance.js
+++ packages/js-math-tools/src/variance.js
@@ -1,38 +1,7 @@
-const assert = require("./assert")
-const flatten = require("./flatten")
-const isArray = require("./is-array")
-const isDataFrame = require("./is-dataframe")
-const isNumber = require("./is-number")
-const isSeries = require("./is-series")
-const mean = require("./mean")
+const stats = require("./stats")
 
 function variance(arr) {
-  if (isDataFrame(arr) || isSeries(arr)) {
-    return variance(arr.values)
-  }
-
-  assert(
-    isArray(arr),
-    "The `variance` function only works on arrays, Series, and DataFrames!",
-  )
-
-  try {
-    const temp = flatten(arr)
-    const m = Number(mean(temp))
-    if (!isNumber(m)) return NaN
-
-    let out = 0
-
-    for (let v of temp) {
-      if (!isNumber(v)) return NaN
-      if (typeof v === "bigint") v = Number(v)
-      out += (v - m) * (v - m)
-    }
-
-    return out / temp.length
-  } catch (e) {
-    return NaN
-  }
+  return stats(arr, { variance: true }).variance
 }
 
 module.exports = variance
