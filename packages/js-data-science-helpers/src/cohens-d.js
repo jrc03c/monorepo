15de658239f4d206258e1e2080dcd3815325639e
jrc03c <jrc03c@pm.me>
2024-09-01 18:44:00 -0500

    ...

--- packages/js-data-science-helpers/src/cohens-d.js
+++ packages/js-data-science-helpers/src/cohens-d.js
@@ -3,9 +3,8 @@ const {
   dropNaN,
   isArray,
   isSeries,
-  mean,
   shape,
-  variance,
+  stats,
 } = require("@jrc03c/js-math-tools")
 
 const common = require("./common")
@@ -24,12 +23,12 @@ function cohensd(arr1, arr2) {
       isArray(arr2) &&
       shape(arr1).length === 1 &&
       shape(arr2).length === 1,
-    "The `cohensd` function only works on 1-dimensional arrays and Series!"
+    "The `cohensd` function only works on 1-dimensional arrays and Series!",
   )
 
   assert(
     arr1.length === arr2.length,
-    "Two arrays or Series passed into the `cohensd` function must have the same length!"
+    "Two arrays or Series passed into the `cohensd` function must have the same length!",
   )
 
   if (common.shouldIgnoreNaNValues) {
@@ -38,10 +37,11 @@ function cohensd(arr1, arr2) {
   }
 
   try {
-    const m1 = mean(arr1)
-    const m2 = mean(arr2)
-    const s = Math.sqrt((variance(arr1) + variance(arr2)) / 2)
-    return (m1 - m2) / s
+    const stats1 = stats(arr1, { variance: true })
+    const stats2 = stats(arr2, { variance: true })
+    const m1 = stats1.mean
+    const m2 = stats2.mean
+    return (m1 - m2) / Math.sqrt((stats1.variance + stats2.variance) / 2)
   } catch (e) {
     return NaN
   }
