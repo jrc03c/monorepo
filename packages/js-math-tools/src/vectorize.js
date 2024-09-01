eb6e6dd8fd3b35a5e7934192c59d950e4e3e0e62
jrc03c <jrc03c@pm.me>
2024-09-01 09:03:17 -0500

    ...

--- packages/js-math-tools/src/vectorize.js
+++ packages/js-math-tools/src/vectorize.js
@@ -9,14 +9,10 @@ const max = require("./max")
 const range = require("./range")
 const shape = require("./shape")
 
-function isArraySeriesOrDataFrame(x) {
-  return isArray(x) || isSeries(x) || isDataFrame(x)
-}
-
 function vectorize(fn) {
   assert(
     isFunction(fn),
-    "You must pass a function into the `vectorize` function!"
+    "You must pass a function into the `vectorize` function!",
   )
 
   return function helper() {
@@ -50,27 +46,25 @@ function vectorize(fn) {
           isArray(s) ? shape(s) : s.shape,
           isArray(childArrays[i + 1])
             ? shape(childArrays[i + 1])
-            : childArrays[i + 1].shape
+            : childArrays[i + 1].shape,
         ),
-        `When passing multiple arrays into the \`${fn.name}\` function, all of the arrays must have the same shape!`
+        `When passing multiple arrays into the \`${fn.name}\` function, all of the arrays must have the same shape!`,
       )
     })
 
     if (childArrays.length > 0) {
       const maxLength = max(
-        childArrays.map(a => (a.length ? a.length : a.values.length))
+        childArrays.map(a => (a.length ? a.length : a.values.length)),
       )
 
       const out = range(0, maxLength).map(i => {
         const args = Object.keys(arguments).map(key => {
-          if (isArraySeriesOrDataFrame(arguments[key])) {
-            if (isArray(arguments[key])) {
-              return arguments[key][i]
-            } else if (isSeries(arguments[key])) {
-              return arguments[key].values[i]
-            } else if (isDataFrame(arguments[key])) {
-              return arguments[key].values[i]
-            }
+          if (isArray(arguments[key])) {
+            return arguments[key][i]
+          } else if (isSeries(arguments[key])) {
+            return arguments[key].values[i]
+          } else if (isDataFrame(arguments[key])) {
+            return arguments[key].values[i]
           } else {
             return arguments[key]
           }
