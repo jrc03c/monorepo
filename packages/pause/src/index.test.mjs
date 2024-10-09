242b014caa8890637844729a2d2316fa18c157ce
jrc03c <jrc03c@pm.me>
2024-10-09 09:37:51 -0500

    ...

---
+++ packages/pause/src/index.test.mjs
@@ -0,0 +1,63 @@
+import { pauseAsync, pauseSync } from "./index.mjs"
+
+let syncTestsAreRunning = true
+
+!(async () => {
+  while (syncTestsAreRunning) {
+    await pauseAsync(100)
+  }
+
+  const start = new Date()
+
+  for (let i = 0; i < 100; i++) {
+    const t = Math.floor(Math.random() * 100)
+    const elapsed = await pauseAsync(t)
+
+    if (typeof elapsed !== "number" || isNaN(elapsed)) {
+      throw new Error(
+        `The function was supposed to return a number representing the number of milliseconds that elapsed since the function was called, but instead it returned a ${typeof elapsed} value! (${elapsed})`,
+      )
+    }
+
+    if (elapsed < t) {
+      throw new Error(
+        `The function was supposed to wait for ${t}ms, but instead it only waited for ${elapsed}ms!`,
+      )
+    }
+  }
+
+  const seconds = (new Date() - start) / 1000
+
+  console.log(
+    `🎉 All asynchronous tests passed! (${seconds.toFixed(2)} seconds)`,
+  )
+})()
+
+!(() => {
+  const start = new Date()
+
+  for (let i = 0; i < 100; i++) {
+    const t = Math.floor(Math.random() * 100)
+    const elapsed = pauseSync(t)
+
+    if (typeof elapsed !== "number" || isNaN(elapsed)) {
+      throw new Error(
+        `The function was supposed to return a number representing the number of milliseconds that elapsed since the function was called, but instead it returned a ${typeof elapsed} value! (${elapsed})`,
+      )
+    }
+
+    if (elapsed < t) {
+      throw new Error(
+        `The function was supposed to wait for ${t}ms, but instead it only waited for ${elapsed}ms!`,
+      )
+    }
+  }
+
+  const seconds = (new Date() - start) / 1000
+
+  console.log(
+    `🎉 All synchronous tests passed! (${seconds.toFixed(2)} seconds)`,
+  )
+
+  syncTestsAreRunning = false
+})()
