fa783b46108b5f4d9a1308e1a2330e69ee519b89
jrc03c <jrc03c@pm.me>
2024-10-09 08:36:45 -0500

    ...

---
+++ packages/create-high-dpi-canvas/dist/create-high-dpi-canvas.require.cjs
@@ -0,0 +1,41 @@
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+
+// src/index.mjs
+var src_exports = {};
+__export(src_exports, {
+  default: () => src_default
+});
+module.exports = __toCommonJS(src_exports);
+function createHighDPICanvas(width, height) {
+  width = Math.floor(width);
+  height = Math.floor(height);
+  let dpi = window.devicePixelRatio || 1;
+  let canvas = document.createElement("canvas");
+  canvas.width = Math.floor(width * dpi);
+  canvas.height = Math.floor(height * dpi);
+  canvas.style.width = `${width}px`;
+  canvas.style.height = `${height}px`;
+  let context = canvas.getContext("2d");
+  context.scale(dpi, dpi);
+  return canvas;
+}
+if (typeof window !== "undefined") {
+  window.createHighDPICanvas = createHighDPICanvas;
+}
+var src_default = createHighDPICanvas;
---
+++ packages/create-high-dpi-canvas/dist/create-high-dpi-canvas.standalone.cjs
@@ -0,0 +1,20 @@
+(() => {
+  // src/index.mjs
+  function createHighDPICanvas(width, height) {
+    width = Math.floor(width);
+    height = Math.floor(height);
+    let dpi = window.devicePixelRatio || 1;
+    let canvas = document.createElement("canvas");
+    canvas.width = Math.floor(width * dpi);
+    canvas.height = Math.floor(height * dpi);
+    canvas.style.width = `${width}px`;
+    canvas.style.height = `${height}px`;
+    let context = canvas.getContext("2d");
+    context.scale(dpi, dpi);
+    return canvas;
+  }
+  if (typeof window !== "undefined") {
+    window.createHighDPICanvas = createHighDPICanvas;
+  }
+  var src_default = createHighDPICanvas;
+})();
