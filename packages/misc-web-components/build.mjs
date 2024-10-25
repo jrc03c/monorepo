645257b10bcd47600f497250ca5f397421ca2539
jrc03c <jrc03c@pm.me>
2024-10-25 16:37:15 -0500

    ...

---
+++ packages/misc-web-components/build.mjs
@@ -0,0 +1,31 @@
+import { execSync } from "node:child_process"
+
+function rebuild() {
+  console.log("-----")
+  console.log(`\nRebuilding... (${new Date().toLocaleString()})`)
+
+  try {
+    const baseCommand = "npx esbuild src/index.mjs --bundle"
+
+    const commands = [
+      "mkdir -p dist",
+      "rm -rf dist/*",
+      `${baseCommand} --platform=node --outfile=dist/misc-vue-components.require.cjs`,
+      `${baseCommand} --platform=node --outfile=dist/misc-vue-components.require.min.cjs --minify`,
+      `${baseCommand} --outfile=dist/misc-vue-components.standalone.cjs`,
+      `${baseCommand} --outfile=dist/misc-vue-components.standalone.min.cjs --minify`,
+      `${baseCommand} --format=esm --outfile=dist/misc-vue-components.import.mjs`,
+      `${baseCommand} --format=esm --outfile=dist/misc-vue-components.import.min.mjs --minify`,
+    ]
+
+    commands.forEach(command => {
+      execSync(command, { encoding: "utf8" })
+    })
+
+    console.log("\nDone! 🎉\n")
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+rebuild()
