0720b6b8e1ad396b5070bafc535c8f898ff51224
jrc03c <jrc03c@pm.me>
2024-11-29 10:55:34 -0600

    ...

--- packages/breakable-promise/src/index.test.mjs
+++
@@ -1,7 +0,0 @@
-import { BreakablePromise } from "./index.mjs"
-
-test("tests that the `BreakablePromise` class works as expected", async () => {
-  if (BreakablePromise) {
-    throw new Error("No tests have been written yet!")
-  }
-})
