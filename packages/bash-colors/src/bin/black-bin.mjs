6fe87a6a028ab1700b3cbaf5b857c377247cb7f4
jrc03c <jrc03c@pm.me>
2025-01-15 10:57:08 -0600

    ...

---
+++ packages/bash-colors/src/bin/black-bin.mjs
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+import { fg } from "../index.mjs"
+import process from "node:process"
+
+const text = process.argv.slice(2).join(" ")
+console.log(fg.black(text))
---
+++ packages/bash-colors/src/bin/bright-bin.mjs
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+import { fx } from "../index.mjs"
+import process from "node:process"
+
+const text = process.argv.slice(2).join(" ")
+console.log(fx.bright(text))
