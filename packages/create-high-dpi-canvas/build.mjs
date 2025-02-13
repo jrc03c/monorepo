684a05410b8c0217ee635dc3f23f2e4a7da2b115
jrc03c <jrc03c@pm.me>
2025-02-13 16:36:40 -0600

    ...

--- packages/create-high-dpi-canvas/build.mjs
+++ packages/create-high-dpi-canvas/build.mjs
@@ -13,7 +13,7 @@ function rebuild() {
       "mkdir -p dist",
       "rm -rf dist/*",
       `${baseCommand} --platform=node --outfile=dist/create-high-dpi-canvas.require.cjs`,
-      `${baseCommand} --outfile=dist/create-high-dpi-canvas.standalone.cjs`,
+      `${baseCommand} --outfile=dist/create-high-dpi-canvas.standalone.js`,
       `${baseCommand} --format=esm --outfile=dist/create-high-dpi-canvas.import.mjs`,
     ]
 
