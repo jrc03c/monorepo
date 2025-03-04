6d9b5869be94b5fd4e3c0eb6f1ea5401d2a9de1e
jrc03c <jrc03c@pm.me>
2025-03-04 12:06:06 -0600

    ...

--- packages/bee/dist/bee.js
+++ packages/bee/dist/bee.js
@@ -6,8 +6,8 @@
   };
 
   // src/index.mjs
-  var src_exports = {};
-  __export(src_exports, {
+  var index_exports = {};
+  __export(index_exports, {
     Drone: () => Drone,
     Queen: () => Queen,
     isInWebWorker: () => isInWebWorker
@@ -1616,7 +1616,7 @@
     }
     const maxRows = typeof window === "undefined" ? 20 : 10;
     const halfMaxRows = Math.floor(maxRows / 2);
-    const maxColumns = typeof process === "undefined" ? 10 : Math.floor(process.stdout.columns / 24) - 1;
+    const maxColumns = 4;
     const halfMaxColumns = Math.floor(maxColumns / 2);
     const tempRows = maxRows > df.index.length ? null : range(0, halfMaxRows).concat(
       range(df.index.length - halfMaxRows, df.index.length)
@@ -4211,6 +4211,6 @@
 
   // src/iife.mjs
   if (typeof globalThis !== "undefined") {
-    globalThis.Bee = src_exports;
+    globalThis.Bee = index_exports;
   }
 })();
