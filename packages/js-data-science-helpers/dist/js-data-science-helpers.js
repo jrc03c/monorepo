d38679a3f201cf0d91881e8af7e90a07d3d1745b
jrc03c <jrc03c@pm.me>
2025-03-04 10:53:06 -0600

    ...

--- packages/js-data-science-helpers/dist/js-data-science-helpers.js
+++ packages/js-data-science-helpers/dist/js-data-science-helpers.js
@@ -6,8 +6,8 @@
   };
 
   // src/index.mjs
-  var src_exports = {};
-  __export(src_exports, {
+  var index_exports = {};
+  __export(index_exports, {
     IndexMatcher: () => IndexMatcher2,
     KMeans: () => KMeans,
     MathError: () => MathError,
@@ -1639,7 +1639,7 @@
     }
     const maxRows = typeof window === "undefined" ? 20 : 10;
     const halfMaxRows = Math.floor(maxRows / 2);
-    const maxColumns = typeof process === "undefined" ? 10 : Math.floor(process.stdout.columns / 24) - 1;
+    const maxColumns = 4;
     const halfMaxColumns = Math.floor(maxColumns / 2);
     const tempRows = maxRows > df.index.length ? null : range(0, halfMaxRows).concat(
       range(df.index.length - halfMaxRows, df.index.length)
@@ -6074,6 +6074,6 @@
 
   // src/iife.mjs
   if (typeof globalThis !== "undefined") {
-    globalThis.JSDataScienceHelpers = src_exports;
+    globalThis.JSDataScienceHelpers = index_exports;
   }
 })();
