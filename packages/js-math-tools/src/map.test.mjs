8fe8f8ff1579dc53c51e2cb39c4fd827ad261982
jrc03c <jrc03c@pm.me>
2025-05-17 21:58:44 -0500

    ...

--- packages/js-math-tools/src/map.test.mjs
+++ packages/js-math-tools/src/map.test.mjs
@@ -1,6 +1,9 @@
+import { DataFrame, Series } from "./dataframe/index.mjs"
+import { distance } from "./distance.mjs"
 import { expect, test } from "@jrc03c/fake-jest"
 import { isEqual } from "./is-equal.mjs"
 import { map } from "./map.mjs"
+import { normal } from "./normal.mjs"
 import { range } from "./range.mjs"
 import { time } from "./time.mjs"
 
@@ -9,6 +12,16 @@ test("tests that the `map` function works as expected", () => {
   const ytrue = x.map(v => v * v)
   const ypred = map(x, v => v * v)
   expect(isEqual(ypred, ytrue)).toBe(true)
+
+  const a = new DataFrame(normal([10, 10]))
+  const btrue = new DataFrame(a.values.map(row => row.map(v => v * 2)))
+  const bpred = map(a, row => row.values.map(v => v * 2))
+  expect(distance(bpred, btrue)).toBeCloseTo(0)
+
+  const c = new Series(normal(100))
+  const dtrue = new Series(c.values.map(v => v * v))
+  const dpred = map(c, v => v * v)
+  expect(distance(dpred, dtrue)).toBeCloseTo(0)
 })
 
 test(
