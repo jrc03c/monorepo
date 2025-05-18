3cb6539c97603c36a99b9507922e1c381e95b299
jrc03c <jrc03c@pm.me>
2025-05-17 21:48:31 -0500

    ...

--- packages/js-math-tools/src/filter.test.mjs
+++ packages/js-math-tools/src/filter.test.mjs
@@ -1,6 +1,8 @@
+import { DataFrame, Series } from "./dataframe/index.mjs"
 import { expect, test } from "@jrc03c/fake-jest"
 import { filter } from "./filter.mjs"
 import { isEqual } from "./is-equal.mjs"
+import { normal } from "./normal.mjs"
 import { range } from "./range.mjs"
 import { time } from "./time.mjs"
 
@@ -9,6 +11,16 @@ test("tests that the `filter` function works as expected", () => {
   const ytrue = x.filter(v => v > 0.5)
   const ypred = filter(x, v => v > 0.5)
   expect(isEqual(ypred, ytrue)).toBe(true)
+
+  const c = new DataFrame(normal([10, 10]))
+  const dtrue = c.values.filter(v => v[0] > 0)
+  const dpred = filter(c, v => v.values[0] > 0).values
+  expect(isEqual(dpred, dtrue)).toBe(true)
+
+  const e = new Series(normal(100))
+  const ftrue = e.values.filter(v => v < 0)
+  const fpred = filter(e, v => v < 0).values
+  expect(isEqual(fpred, ftrue)).toBe(true)
 })
 
 test(
